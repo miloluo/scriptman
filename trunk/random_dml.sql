ebfe0226eebc1f5ff2a0ecfffad10edbf8c8ff6d
lxybhbh@gmail.com <lxybhbh@gmail.com@44276fca-700e-e68a-3f03-c94f677be5fe>
2013-10-31 16:50:08 +0000

    fix the ora-00905 missing keyword issue due to 'select count(1) into rowcnt ...' should correct to "sqlstmt := 'select count(1) from perf.perf_tab1 where col1 >= :1 and col1 <= :2';" and "execute immediate sqlstmt into rowcnt using lowkey, highkey;"

--- trunk/random_dml.sql
+++ trunk/random_dml.sql
@@ -27,27 +27,17 @@ begin
         -- ################################################
 
         dbms_output.put_line(chr(10)||'++++++++++++++++++++++++++++++++++++++++++++++++');
-
         opcode := abs(mod(dbms_random.random,3));
-        --dbms_output.put_line('OP code is : ' || opcode);
-
 
         -- ################################################
         -- # 2. execute the dml
         -- ################################################
 
-
+        -- generate randkey
         randkey := abs(mod(dbms_random.random,1000))+1 ;
-
-        --dbms_output.put_line(randkey);
-
-
         lowkey := randkey - rows_before_after;
         highkey := randkey + rows_before_after;
 
-        --dbms_output.put_line(lowkey);    
-        --dbms_output.put_line(highkey);
-    
         -- Initialize row count
         rowcnt := -1;
 
@@ -58,7 +48,6 @@ begin
 
             -- I know the rowcnt
             select count(*) into rowcnt from perf.deldata1;
-            --execute immediate sqlstmt into rowcnt using lowkey, highkey;
 
             proc_del_data_back('perf_tab1','deldata1');
             dbms_output.put_line('Loop ' || i || ' : Insert '  || ' -> ' || rowcnt || ' records.' );
@@ -73,11 +62,12 @@ begin
             
             -- I know the rowcnt
             sqlstmt := 'select count(*) from perf.perf_tab1 where col1 >= :2 and col1 <= :3';
+	    --dbms_output.put_line(sqlstmt || chr(10) || 'rowcnt: ' || rowcnt || chr(10) || 'lowkey: ' || lowkey || chr(10) || 'highkey: ' || highkey);
             execute immediate sqlstmt into rowcnt using lowkey, highkey;
                    
-            -- try max_tries times to see if there is a none empty resultset.   
+
             try_flag := 0;
-                
+            -- try max_tries times to see if there is a none empty resultset.   
             for j in 1..max_tries loop  
                   
                if rowcnt > 0 then
@@ -94,41 +84,31 @@ begin
                   
                -- generate randkey   
                randkey := abs(mod(dbms_random.random,1000))+1 ;
-               
                lowkey := randkey - rows_before_after;
                highkey := randkey + rows_before_after;
 
                -- I know the rowcnt
-               sqlstmt := 'select count(1) into rowcnt from perf.perf_tab1 where col1 >= :1 and col1 <= :2';
+               sqlstmt := 'select count(1)  from perf.perf_tab1 where col1 >= :1 and col1 <= :2';
+	       --dbms_output.put_line(sqlstmt || chr(10) || 'rowcnt: ' || rowcnt || chr(10) || 'lowkey: ' || lowkey || chr(10) || 'highkey: ' || highkey);
                execute immediate sqlstmt into rowcnt using lowkey, highkey;
             
             end loop;
             
-            -- if update then do nothing
-            if try_flag = 1 then 
-                 null;
-                 
             -- if no resultset match after $max_tries time, then output update failed     
-            else 
+            if try_flag = 0 then 
                   dbms_output.put_line('Loop ' || i || ' : Update failed after try ' ||  max_tries || ' times!');
                   dbms_output.put_line('++++++++++++++++++++++++++++++++++++++++++++++++'); 
             end if;
             
             
-
-            
         -- Delete (opcode = 2)
         elsif opcode = 2 then
 
 -- 添加判断是否结果集为0，如果为0，调用其他值，调用有个次数限制，如果超过该值，操作改为insert或报出delete失败。         
---            execute immediate 'delete from perf.perf_tab1 where col1 >= ' || lowkey || ' and col1 <= ' || highkey ;
---            commit;
---            dbms_output.put_line('Loop ' || i || ' : Delete ');
---            dbms_output.put_line('++++++++++++++++++++++++++++++++++++++++++++++++');
 
 	    -- I know the rowcnt
             sqlstmt := 'select count(*) from perf.perf_tab1 where col1 >= :2 and col1 <= :3';
-            --sqlstmt := 'select count(*) into rowcnt from perf.perf_tab1 where col1 >= :1 and col1 <= :2';
+	    --dbms_output.put_line(sqlstmt || chr(10) || 'rowcnt: ' || rowcnt || chr(10) || 'lowkey: ' || lowkey || chr(10) || 'highkey: ' || highkey);
             execute immediate sqlstmt into rowcnt using lowkey, highkey;
 
             -- try max_tries times to see if there is a none empty resultset.   
@@ -153,11 +133,17 @@ begin
                highkey := randkey + rows_before_after;
                
                -- I know the rowcnt
-               sqlstmt := 'select count(1) into rowcnt from perf.perf_tab1 where col1 >= :1 and col1 <= :2';
+               sqlstmt := 'select count(1) from perf.perf_tab1 where col1 >= :1 and col1 <= :2';
+	       --dbms_output.put_line(sqlstmt || chr(10) || 'rowcnt: ' || rowcnt || chr(10) || 'lowkey: ' || lowkey || chr(10) || 'highkey: ' || highkey);
                execute immediate sqlstmt into rowcnt using lowkey, highkey;
             
             end loop;
                
+            -- if no resultset match after $max_tries time, then output delete failed     
+            if try_flag = 0 then 
+                  dbms_output.put_line('Loop ' || i || ' : Delete failed after try ' ||  max_tries || ' times!');
+                  dbms_output.put_line('++++++++++++++++++++++++++++++++++++++++++++++++'); 
+            end if;
 
         -- exception
         else
@@ -166,10 +152,7 @@ begin
 
         end if;
         
-
-
     end loop;
-
 
 end;
 /
